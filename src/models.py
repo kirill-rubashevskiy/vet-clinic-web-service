a9cffa9ac2f7ea4e71a689b3a32b3f4948ab0821
Kirill Rubashevskiy <kirill.rubashevskiy@gmail.com>
2023-10-28 23:29:06 +0300

    - add PostgresSQL integration

---
+++ src/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+
+from src.database import Base
+
+
+class Dog(Base):
+    __tablename__ = "dogs"
+
+    name = Column(String)
+    pk = Column(Integer, primary_key=True, unique=True)
+    kind = Column(String)
+
+
+class Timestamp(Base):
+    __tablename__ = "timestamps"
+
+    id = Column(Integer, primary_key=True, unique=True)
+    timestamp = Column(Integer)
